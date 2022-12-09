45a11917785cc51b02946a520727250d2c5d3e87
NomoteteS <nomotetes.onetrue@icloud.com>
2022-12-09 14:05:12 +0200

    Initial Commit

---
+++ LoginPageWithFirebaseMVVM/LoginPageWithFirebaseMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageWithFirebaseMVVMApp.swift
+//  LoginPageWithFirebaseMVVM
+//
+//  Created by NomoteteS on 9.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageWithFirebaseMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
